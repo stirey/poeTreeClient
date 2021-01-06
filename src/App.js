1bccc24226b3a2198352e39857d46b1f58d8de2b
Sarah Tirey <sarah@email.com>
2021-01-05 21:39:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import About from './components/site/About';
+import Banner from './components/site/Banner';
+import Contact from './components/site/Contact';
+import Footer from './components/site/Footer';
+import Header from './components/site/Header';
+import Auth from './components/auth/Auth';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/site/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Banner() {
+    return (
+      <div className="App">
+        test
+      </div>
+    );
+  }
+  
+  export default Banner;
